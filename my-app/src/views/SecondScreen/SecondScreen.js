1f102a104e0dc509916f86c5357bc8db136d7acb
Dani <masone.daniele@gmail.com>
2019-03-22 16:51:05 +0100

    bugfix

--- my-app/src/views/SecondScreen/SecondScreen.js
+++ my-app/src/views/SecondScreen/SecondScreen.js
@@ -7,42 +7,39 @@ import './SecondScreen-style.css';
 window.__DEV__ = true;
 
 class SecondScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      obj: [
+        {
+          title: "A",
+          body: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        },
+        {
+          title: "B",
+          body: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        },
+        {
+          title: "C",
+          body: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        }
+        ]
+    }
+  }
+
   render() {
     return (
       <div className="SecondScreen">
-        <Media className="SecondScreen-header"> 
-            <img src={logo} className="SecondScreen-logo align-self-center mr-3" alt="logo"/>
-            <Media.Body>
-              <h5>TITOLO 1</h5>
-              <p>
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA.
-              </p>
-            </Media.Body>
-          </Media>
-          <Media className="SecondScreen-header"> 
-            <img src={logo} className="SecondScreen-logo align-self-center mr-3" alt="logo"/>
-            <Media.Body>
-              <h5>TITOLO 1</h5>
-              <p>
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA.
-              </p>
-            </Media.Body>
-          </Media>
-          <Media className="SecondScreen-header"> 
-            <img src={logo} className="SecondScreen-logo align-self-center mr-3" alt="logo"/>
-            <Media.Body>
-              <h5>TITOLO 1</h5>
-              <p>
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
-                BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA.
-              </p>
-            </Media.Body>
-          </Media>
+          {this.state.obj.map((item) =>
+            <Media className="SecondScreen-header"> 
+              <img src={logo} className="SecondScreen-logo align-self-center mr-3" alt="logo"/>
+                <Media.Body>
+                <h5>{item.title}</h5>
+                <p>{item.body}</p>
+              </Media.Body>
+            </Media>
+          )}
       </div>
     );
   }
